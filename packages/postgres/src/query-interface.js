7b0981f3a95106f9d953c90ee1473e070c56c7f8
Andreas Lind <andreas.lind@workday.com>
2026-05-07 23:17:51 +0200

    Self-review
    (cherry picked from commit 4d1baacd30901b8998ca24a001bd78ccb0b48c1f)

--- packages/postgres/src/query-interface.js
+++ packages/postgres/src/query-interface.js
@@ -321,11 +321,18 @@ export class PostgresQueryInterface extends PostgresQueryInterfaceTypescript {
     const attributes = model.modelDefinition.attributes;
 
     for (const attribute of attributes.values()) {
-      if (!(attribute.type instanceof DataTypes.ENUM)) {
+      const enumType =
+        attribute.type instanceof DataTypes.ARRAY &&
+        attribute.type.options.type instanceof DataTypes.ENUM
+          ? attribute.type.options.type
+          : attribute.type instanceof DataTypes.ENUM
+            ? attribute.type
+            : null;
+
+      if (enumType === null) {
         continue;
       }
 
-      const enumType = attribute.type;
       let sql;
 
       if (enumType.options.name) {
@@ -340,6 +347,8 @@ export class PostgresQueryInterface extends PostgresQueryInterfaceTypescript {
                 ? otherAttr.type.options.type
                 : otherAttr.type;
 
+            // Note: undefined schema (use table's default) and an explicit 'public'
+            // are treated as different here even if they resolve to the same schema.
             return (
               otherType instanceof DataTypes.ENUM &&
               otherType.options.name === enumType.options.name &&
